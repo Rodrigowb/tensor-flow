01e4b7a14c6f02de41fe0dadf0986986f80173ea
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2022-09-02 08:19:51 -0300

    trying to improve our model and learning that the testing data metrics are not important, because can be overfitted. The important is predictions with data that the model hasn't seen before. The learning rate is the most important parameter to change...

--- classes/01_neural_network_regression_in_tensorflow.py
+++ classes/01_neural_network_regression_in_tensorflow.py
@@ -14,13 +14,13 @@ Predicting a numerical variable based o some other combinations of variables (Pr
 
 -----Steps in modeling with TensorFlow-----
 1- Creating a model: define the input, hidden and output layers.
-2- Compiling a model: define the loss function (tells how wrong the model is) and the optimizer 
+2- Compiling a model: define the loss function (tells how wrong the model is) and the optimizer
 how to improve the patterns) and evaluating metrics (what we can use to interpret the performance of our model).
 3- Fitting a model: letting the model try to find patterns between x and y (features and labels).
 
 -----Steps to improve our model-----
 Altering the steps to create a model
-1- Creating a model: 
+1- Creating a model:
 a) Add more layers
 b) Increase numbers of hidden layers (neurons)
 c) Change activation function for each layer
@@ -30,7 +30,7 @@ a) Change the optimization function and learning rate of the optimization functi
 
 3- Fitting the model:
 a) Set more epochs to the fitting (leave it training longer)
-b) Get more data to the model train 
+b) Get more data to the model train
 """
 
 import tensorflow as tf
@@ -69,7 +69,7 @@ def FirstModel():
                   metrics=["mae"])
 
     # 3. Fit the model (after it, you have a trained model)
-    model.fit(tf.expand_dims(X, axis=-1), Y, epochs=10)
+    model.fit(tf.expand_dims(X, axis=-1), Y, epochs=1000)
 
     # 4. Try and make a prediction using the model
     prediction = model.predict([17.0])
@@ -81,22 +81,25 @@ def FirstModel():
 def SecondModel():
     """
     Improving the FirstModel()
-    1- Improve epochs
+    1- Add epochs
     2- Adding hidden layers
-    3- Changing the optimizers for adam
+    3- Increase the number of hidden units
+    4- Change the activation function
+    5- Change the optimization function
+    6- Change the learning rate
     """
     # 1- Create the model
     model = tf.keras.Sequential([
-        tf.keras.layers.Dense(100, activation="relu"),
-        tf.keras.layers.Dense(100, activation="relu"),
-        tf.keras.layers.Dense(100, activation="relu"),
+        tf.keras.layers.Dense(50, activation=None),
+        # tf.keras.layers.Dense(100, activation="relu"),
+        # tf.keras.layers.Dense(100, activation="relu"),
         tf.keras.layers.Dense(1)
 
     ])
 
     # 2- Compiling the model
     model.compile(loss=tf.keras.losses.mae,
-                  optimizer=tf.keras.optimizers.Adam(lr=0.0001),
+                  optimizer=tf.keras.optimizers.Adam(lr=0.01),
                   metrics=["mae"])
 
     # 3- Fit the model
@@ -106,5 +109,8 @@ def SecondModel():
     prediction = model.predict([17.0])
     print(prediction)
 
+    # !!!Producing overfitting: the metrics in the training data is not the key parameters to evaluate a model!!!
+
 
 SecondModel()
+# FirstModel()
