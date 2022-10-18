64f6a3a2dd52c234fed4d1a13e49c6424714a7d5
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2022-10-18 09:36:29 -0300

    integrate with binance, ccxt and creating a service folder to create helper functions to evaluate ml models

--- classes/02_neural_network_classification_in_tensorflow.py
+++ classes/02_neural_network_classification_in_tensorflow.py
@@ -38,14 +38,27 @@
 -----Linear activation functions-----
 1- Linear: does not change the input data
 
+-----Classification evaluation methods-----
+1- Accuracy: default for classification
+2- Precision
+3- Recall
+Obs: tradeoff between precision and recall
+4- F1score: combination of precision and recall
+5- Confusion matrix
+
 """
 
+from gc import callbacks
 from turtle import circle
 from sklearn.datasets import make_circles
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import numpy as np
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+import itertools
+from services import MachineLearningMetrics
 
 # Create a visualization function
 
@@ -88,6 +101,79 @@ def plot_decision_boundary(model, X, Y):
     plt.show()
 
 
+def pretty_confusion_matrix(y_test, y_pred):
+    figsize = (10, 10)
+
+    # Create the confusion matrix
+    cm = confusion_matrix(y_test, y_pred)
+    cm_norm = cm.astype("float") / \
+        cm.sum(axis=1)[:, np.newaxis]  # normalize it
+    n_classes = cm.shape[0]
+
+    # Let's prettify it
+    fig, ax = plt.subplots(figsize=figsize)
+    # Create a matrix plot
+    # https://matplotlib.org/3.2.0/api/_as_gen/matplotlib.axes.Axes.matshow.html
+    cax = ax.matshow(cm, cmap=plt.cm.Blues)
+    fig.colorbar(cax)
+
+    # Create classes
+    classes = False
+
+    if classes:
+        labels = classes
+    else:
+        labels = np.arange(cm.shape[0])
+
+    # Label the axes
+    ax.set(title="Confusion Matrix",
+           xlabel="Predicted label",
+           ylabel="True label",
+           xticks=np.arange(n_classes),
+           yticks=np.arange(n_classes),
+           xticklabels=labels,
+           yticklabels=labels)
+
+    # Set x-axis labels to bottom
+    ax.xaxis.set_label_position("bottom")
+    ax.xaxis.tick_bottom()
+
+    # Adjust label size
+    ax.xaxis.label.set_size(20)
+    ax.yaxis.label.set_size(20)
+    ax.title.set_size(20)
+
+    # Set threshold for different colors
+    threshold = (cm.max() + cm.min()) / 2.
+
+    # Plot the text on each cell
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > threshold else "black",
+                 size=15)
+    #  Plot the graph
+    plt.show()
+
+
+def plot_loss_curve(history):
+    pd.DataFrame(history.history).plot(xlabel="epochs")
+    plt.title("Model training curve")
+    plt.show()
+
+
+def plot_loss_vs_learning(history):
+    """Find the ideal learning rate: where the loss istill decreasing, but not flatten out"""
+    lrs = 1e-4 * (10 ** (np.arange(100)/20))
+    plt.figure(figsize=(10, 7))
+    # we want the x-axis (learning rate) to be log scale
+    plt.semilogx(lrs, history.history["loss"])
+    plt.xlabel("Learning Rate")
+    plt.ylabel("Loss")
+    plt.title("Learning rate vs. loss")
+    plt.show()
+
+
 def Introduction():
     # Create data to view and fit
     n_samples = 1000
@@ -97,17 +183,17 @@ def Introduction():
                         noise=0.03,
                         random_state=42)
 
-    print(X.shape, Y.shape)
-
     # Better visualize the data as a df
     circles = pd.DataFrame({"X0": X[:, 0], "X1": X[:, 1], "label": Y})
-    # print(circles)
 
     # Plot a graph to understand
     plt.scatter(X[:, 0], X[:, 1], c=Y)
-    # plt.show()
 
-    # Set seed
+    # Split the data
+    x_train, x_test, y_train, y_test = train_test_split(
+        X, Y, test_size=0.2, random_state=42)
+
+    # # Set seed
     tf.random.set_seed(42)
 
     # 1- Create the model
@@ -119,17 +205,39 @@ def Introduction():
 
     # 2- Compile the model
     model.compile(loss=tf.keras.losses.BinaryCrossentropy(),
-                  optimizer=tf.keras.optimizers.Adam(),
+                  optimizer=tf.keras.optimizers.Adam(lr=0.01),
                   metrics=["accuracy"])
 
+    # Create a learning rate scheduler callback
+    lr_scheduler = tf.keras.callbacks.LearningRateScheduler(
+        lambda epoch: 1e-4 * 10**(epoch/20))
+
     # 3- Fit the model
-    model.fit(X, Y, epochs=150, verbose=1)
+    history = model.fit(x_train, y_train, epochs=100,
+                        verbose=1, callbacks=[lr_scheduler])
 
     # 4 & 5- Tweak and evaluate the model
-    model.evaluate(X, Y)
+    loss, accuracy = model.evaluate(x_test, y_test)
+    print(f"Model loss on the test set: {loss}")
+    print(f"Model accuracy on the test set: {100*accuracy:.2f}%")
+
+    # Make predictions
+    prediction = model.predict(x_test)
+
+    # Plot the loss curve (how the model is behaving while learning?)
+    plot_loss_curve(history)
+    plot_loss_vs_learning(history)
+    print(pd.DataFrame(history.history))
+
+    # Transform the predictions into binary
+    prediction_binary = tf.round(prediction)
+
+    # Create a confusion matrix
+    pretty_confusion_matrix(y_test, prediction_binary)
 
     # Check out the predictions our model is making
-    plot_decision_boundary(model, X, Y)
+    # plot_decision_boundary(model, x_test, y_test)
+    # mlm.plot_decision_boundary(model, x_test, y_test)
 
 
 Introduction()
