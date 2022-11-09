5b15f0a6ca4bf87844ea4ae94bf45eec7454190a
boaventurarodrigo@yahoo.com.br <boaventurarodrigo@yahoo.com.br>
2022-11-09 10:39:10 -0300

    building our first CNN mode. First normalize the data, between 0 and 1, dividing the tensors into 255, wich is the largest hex value for pixels. Than, set up the directory path that the images are, then, transform the data into batches to manage memory in our computer and enable our model to run. After it, create the model using Sequential mode and layers (Conv2D, MaxPool and Flatten) and the output layer, the Dense. After, compile the model using BinaryCrossentropy(), because is a binary classification model and fit it into batches

--- classes/03_introduction_to_computer_vision_with_tensorflow_video.py
+++ classes/03_introduction_to_computer_vision_with_tensorflow_video.py
@@ -1,9 +1,26 @@
-"""Introduction to Convolution Neural Networks and Compuer Vision with Tensorflow"""
+"""
+Introduction to Convolution Neural Networks and Compuer Vision with Tensorflow
+
+-----Steps into modeling CNN-----
+1- Load the images
+2- Preprocess the images
+3- Build a CNN to find patterns in the image
+4- Compile the CNN
+5- Fit the CNN to our training data
+
+-----Batches-----
+Control memory in the computer.
+"""
+
+from multiprocessing import pool
 import os
+from pickletools import optimize
+from unittest import result
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import random
 import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
 def directory_details(dir_path):
@@ -25,7 +42,49 @@ def view_random_image(target_folder):
     return tf.constant(img)
 
 
+def first_cnn():
+    # Set random seed
+    tf.random.set_seed(42)
+
+    # Preprocess data
+    train_datagen = ImageDataGenerator(rescale=1./255)
+    test_datagen = ImageDataGenerator(rescale=1./255)
+
+    # Set up paths for the directories
+    train_dir = "../services/images/pizza_steak/train"
+    test_dir = "../services/images/pizza_steak/test"
+
+    # Import the data and transform them into batches
+    train_data = train_datagen.flow_from_directory(
+        directory=train_dir, batch_size=32, target_size=(224, 224), class_mode="binary", seed=42)
+
+    valid_data = test_datagen.flow_from_directory(
+        directory=test_dir, batch_size=32, target_size=(224, 224), class_mode="binary", seed=42)
+
+    # Build a CNN model
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(filters=10, kernel_size=3,
+                               activation="relu", input_shape=(224, 224, 3)),
+        tf.keras.layers.Conv2D(10, 3, activation="relu"),
+        tf.keras.layers.MaxPool2D(pool_size=2, padding="valid"),
+        tf.keras.layers.Conv2D(10, 3, activation="relu"),
+        tf.keras.layers.Conv2D(10, 3, activation="relu"),
+        tf.keras.layers.MaxPool2D(2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(1, activation="sigmoid")
+    ])
+
+    # Compile the CNN
+    model.compile(loss=tf.keras.losses.BinaryCrossentropy(),
+                  optimizer=tf.keras.optimizers.Adam(),
+                  metrics=["accuracy"])
+
+    # Fit the CNN model
+    history = model.fit(train_data, epochs=5, steps_per_epoch=len(
+        train_data), validation_data=valid_data, validation_steps=len(valid_data))
+
+
 if __name__ == "__main__":
-    directory_details("../services/images/pizza_steak")
-    image = view_random_image("../services/images/pizza_steak/test/steak")
-    print(image)
+    # directory_details("../services/images/pizza_steak")
+    # image = view_random_image("../services/images/pizza_steak/test/steak")
+    first_cnn()
